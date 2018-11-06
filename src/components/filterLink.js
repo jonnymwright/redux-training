e53164f9d4dfcadf321f0264dc560a390238173a
Jonny Wright <jmwright@scottlogic.co.uk>
2018-11-06 08:42:15 +0000

    Fix style on All Filter link

--- src/components/filterLink.js
+++ src/components/filterLink.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const FilterLink = ({ filter, children }) => (
   <NavLink
+    exact
     to={filter === 'all' ? '' : '/' + filter}
     activeStyle={{
       textDecoration: 'none',
